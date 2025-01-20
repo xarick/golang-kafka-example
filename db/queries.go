937be89268ab6f563d93d56e54b17c1f6c8e9b78
Bobirjon Tohirov <tokhirovbob@gmail.com>
2025-01-20 16:07:10 +0500

    Update queries.go

--- db/queries.go
+++ db/queries.go
@@ -2,6 +2,13 @@ package db
 
 import "github.com/xarick/golang-kafka-example/models"
 
+type UserDB struct {
+	ID        int    `db:"id" json:"id"`
+	FIO       string `db:"fio" json:"fio"`
+	Username  string `db:"username" json:"username"`
+	CreatedAt string `db:"created_at" json:"created_at"`
+}
+
 func RegisterUser(fio, username, hashedPassword string) error {
 	query := "INSERT INTO users (fio, username, password) VALUES ($1, $2, $3)"
 	_, err := DB.Exec(query, fio, username, hashedPassword)
@@ -13,4 +20,11 @@ func GetUserByName(username string) (*models.User, error) {
 	var user models.User
 	err := DB.Get(&user, query, username)
 	return &user, err
+}
+
+func GetUsers() ([]UserDB, error) {
+	var users []UserDB
+	query := "SELECT id, fio, username, created_at FROM users"
+	err := DB.Select(&users, query)
+	return users, err
 }
